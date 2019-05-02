0920b56296e7cf198f9b2f16fd5b46ae5acc92cf
wisdomtooth <cbtsao47@gmail.com>
2019-05-02 03:42:53 -0500

    add Palette

---
+++ src/components/Palete.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Palete extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* navbar here */}
+        <div className="Palette-colors">{/* color boxes */}</div>
+        {/* footer */}
+      </div>
+    );
+  }
+}
